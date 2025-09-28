aeb7a99f6274ed600390915643fa4d2ad34052c5
Lostmaniac2928 <ananthusubhash2928@gmail.com>
2025-09-28 08:51:55 +0530

    jwt cokie send correction server side

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -6,15 +6,25 @@ const api = axios.create({
   baseURL: API_BASE_URL,
 });
 
-// This "interceptor" automatically adds the token to every request
-api.interceptors.request.use((config) => {
-  const userInfo = JSON.parse(localStorage.getItem('userInfo'));
-  if (userInfo && userInfo.token) {
-    config.headers.Authorization = `Bearer ${userInfo.token}`;
+// This interceptor will add the token to every request
+api.interceptors.request.use(
+  (config) => {
+    try {
+      const userInfoString = localStorage.getItem('userInfo');
+      if (userInfoString) {
+        const userInfo = JSON.parse(userInfoString);
+        if (userInfo && userInfo.token) {
+          config.headers.Authorization = `Bearer ${userInfo.token}`;
+        }
+      }
+    } catch (e) {
+      console.error('Error parsing user info from localStorage', e);
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default api;
