2ec2b2cd5a6e3340121fe6b18ff1beadaee65697
Lostmaniac2928 <ananthusubhash2928@gmail.com>
2025-09-28 13:46:42 +0530

    fix loading service

--- server/server.js
+++ server/server.js
@@ -2,39 +2,40 @@ import path from 'path';
 import express from 'express';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
 import connectDB from './config/db.js';
-
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import serviceRoutes from './routes/serviceRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
-
 import { notFound, errorHandler } from './middleware/errorHandler.js';
 
 connectDB();
 const app = express();
-
-app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-// API Routes
+// --- API ROUTES ---
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/services', serviceRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/admin', adminRoutes);
 
-// Deployment Setup
+// --- DEPLOYMENT SETUP ---
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/client/dist')));
-app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
-);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/dist')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
-// Error handling
+// --- ERROR HANDLING ---
 app.use(notFound);
 app.use(errorHandler);
 
